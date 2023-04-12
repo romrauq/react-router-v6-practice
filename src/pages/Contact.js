b37f3f0a3bc8fa265ce6fafd09e0163e6791b988
John Quarmor <johnequarmor@gmail.com>
2023-04-12 17:19:04 +0000

    Created page file for route testing

---
+++ src/pages/Contact.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Services = () => {
+	return <div>This is the Services Page</div>;
+};
+
+export default Services;
---
+++ src/pages/Services.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Services = () => {
+	return <div>This is the Service Page</div>;
+};
+
+export default Services;

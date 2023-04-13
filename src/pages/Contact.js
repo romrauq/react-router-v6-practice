45d8efe78cf5f4cb3563f2c9e77ff67940b0a610
John Quarmor <johnequarmor@gmail.com>
2023-04-13 18:44:32 +0000

    Created page compoent.

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Services = () => {
-	return <div>This is the Services Page</div>;
+	return <div>This is the Contact Us Page</div>;
 };
 
 export default Services;
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Services = () => {
-	return <div>This is the Service Page</div>;
+	return <div>This is Our Service Page</div>;
 };
 
 export default Services;
